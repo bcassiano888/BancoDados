bd0fd75c609d7f41c55084e28cb40a355a8d0168
Cassiano Borba <bcassiano888@gmail.com>
2026-03-31 21:42:27 -0300

    update

--- slide aula 06/rel ternario.sql
+++ slide aula 06/rel ternario.sql
@@ -56,3 +56,63 @@ constraint fk_tb_pesquisas_nr foreign key(nr)
 constraint fk_tb_pesquisas_rg foreign key(rg)
 	references tb_pesquisadores(rg)
 );
+
+-- criando a tb_produtos
+-- restrições:
+-- pk aplicada a coluna codigo
+
+create table tb_produtos(
+codigo 		integer,
+ds_produto 	varchar(45),
+preco 		numeric(7,2),
+constraint pk_tb_produtos_cod primary key(codigo)
+);
+
+-- criando a tb_cidades
+-- restrição:
+--pk aplicada a coluna codigo
+
+create table tb_cidades(
+codigo	integer,
+nm_cidade 	varchar(45),
+uf			char(2),
+
+constraint pk_tb_cidades_cod primary key(codigo)
+);
+
+-- criando a tb_distribuidores
+-- restricão:
+-- pk aplicada a coluna rg
+
+create table tb_distribuidores(
+rg 				varchar(11),
+nm_distribuidor varchar(60),
+dt_nasc 		date,
+
+constraint pk_tb_distribuidores_rg primary key(rg)
+);
+
+-- criando a tb_distribuições 
+-- proveniente do relacionamento 1:N:N
+-- restrições
+-- pk composta aplicada as colunas codproduto e codcidade
+-- fk aplicada a coluna cod_produto
+-- fk aplicada a coluna cod_cidade
+-- fk aplicada a coluna rg
+
+create table tb_distribuicoes(
+cod_produto 	integer,
+cod_cidade 	 	integer,
+rg 				varchar(11),
+
+constraint pk_tb_dist_cod_prod_cod_cid primary key(cod_produto,cod_cidade),
+
+constraint fk_tb_dist_cod_prod foreign key (cod_produto)
+	references tb_produtos(codigo),
+
+constraint fk_tb_dist_cod_cid foreign key (cod_cidade)
+	references tb_cidades(codigo),
+
+constraint fk_tb_dist_rg foreign key(rg)
+	references tb_distribuidores(rg)
+);
